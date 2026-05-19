3f1dfbec3fe59a04ea113f32f260fe92d863fc8e
vaibhav <jhavaibhav419@gmail.com>
2026-05-19 12:35:14 +0530

    fix error

--- server/index.js
+++ server/index.js
@@ -12,8 +12,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
